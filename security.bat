072061dcca8e5064d25430de865eb92e328c9c7c
W9t2333 <51908985+W9t2333@users.noreply.github.com>
2019-10-22 12:53:36 +0800

    Add files via upload

---
+++ security.bat
@@ -0,0 +1,21 @@
+ÿþ
+@echo off
+%1 %2
+ver|find "5.">nul&&goto :Admin
+mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin","","runas",1)(window.close)&goto :eof
+:Admin
+
+sc stop LanmanServer
+sc config LanmanServer start=disabled
+netsh advfirewall firewall add rule name = "Disable port 135 - TCP" dir = in action = block protocol = TCP localport = 135,137,138,139
+netsh advfirewall firewall add rule name = "Disable port 135 - UDP" dir = in action = block protocol = UDP localport = 135,137,138,139
+netsh advfirewall firewall add rule name = "Disable port 445 - TCP" dir = in action = block protocol = TCP localport = 445
+netsh advfirewall firewall add rule name = "Disable port 445 - UDP" dir = in action = block protocol = UDP localport = 445
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Ole" /v EnableDCOM /t REG_SZ /d N /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d 2333 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NetBT\Parameters" /v SMBDeviceEnabled /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\OLE" /v EnableDCOM /t REG_SZ /d N /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Rpc" /v DCOM Protocols /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Rpc\Internet " /f
+
+pause
